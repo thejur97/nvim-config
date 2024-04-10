5c6618a51a700b3814e53a945867f36bc5fdd7d7
thej_raj <thejur97@gmail.com>
2024-04-10 21:30:16 +0530

    added line number to netrw

--- lua/theju/set.lua
+++ lua/theju/set.lua
@@ -8,3 +8,4 @@ vim.opt.termguicolors = true
 vim.opt.listchars = "eol:~,tab:>.,trail:~,extends:>,precedes:<,space:_"
 vim.o.clipboard = "unnamedplus"
 vim.cmd [[colorscheme gruvbox]]
+vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro'
