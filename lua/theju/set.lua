b2cd55a905c6db77aebbf5e27e2a7ce567c24022
theju <thejur97@gmail.com>
2025-06-19 13:43:22 +0530

    deleted coc and added few basic commands

--- lua/theju/set.lua
+++ lua/theju/set.lua
@@ -7,7 +7,11 @@ vim.opt.list = true
 vim.opt.mouse = ""
 vim.g.mapleader = " "
 vim.opt.termguicolors = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 vim.opt.listchars = "eol:~,tab:>.,trail:~,extends:>,precedes:<,space:_"
 vim.o.clipboard = "unnamedplus"
-vim.cmd [[colorscheme gruvbox]]
+vim.cmd [[colorscheme gruvbox8]]
 vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro'
+vim.g.netrw_localcopydircmd = 'cp -r'
